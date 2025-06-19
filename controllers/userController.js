924ac71905b3bd23f0086b68e75813452c3ca99d
punpunslrg <punpun.slrg@gmail.com>
2025-06-19 10:15:07 +0700

    update controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,30 +1,46 @@
-import { createError } from "../utils/createError.js"
+import { createError } from "../utils/createError.js";
+import prisma from "../config/prisma.js";
 
 export async function listUser(req, res, next) {
   try {
-    if(true){
-      createError(400, "Email is already exists")
-    } else {
-      throw new Error('Password is invalid !!')
-    }
-    res.json({ message: "This is List All User" })
+    const user = await prisma.user.findMany({
+      omit: {
+        password: true,
+      },
+    });
+    console.log(user);
+
+    res.json({ message: "This is List All User", result: user });
   } catch (error) {
-    next(error)
+    next(error);
+  }
+}
+
+export async function updateRoleUser(req, res, next) {
+  try {
+    // 1. Read params & body
+    const { id } = req.params
+    const { role } = req.body
+    console.log(id, role)
+
+    res.json({ message: "This is Update Role User" });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function deleteUser(req, res, next) {
+  try {
+    res.json({ message: "This is DELETE User" });
+  } catch (error) {
+    next(error);
   }
 }
 
 export async function readUser(req, res) {
-  res.json({ message: "This is Read User" })
+  res.json({ message: "This is Read User" });
 }
 
 export async function createUser(req, res) {
-  res.json({ message: "This is POST User" })
-}
-
-export async function updateRoleUser(req, res) {
-  res.json({ message: "This is Update Role User" })
-}
-
-export async function deleteUser(req, res) {
-  res.json({ message: "This is DELETE User" })
+  res.json({ message: "This is POST User" });
 }
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,6 +1,38 @@
+import { createError } from "../utils/createError.js";
+import jwt from "jsonwebtoken";
+
 export async function authCheck(req, res, next) {
-  if(false) {
-    console.log("This is middleware")
+  try {
+    /*
+      1. Check header
+      2. Split Token Bearer
+      3. Verify JWT
+      4. Create req.user
+    */
+
+    // 1. Check header
+    const header = req.headers.authorization;
+    // console.log(header)
+    if (!header) {
+      createError(401, "Token is missing, authorization denied");
+    }
+
+    // console.log("headers from middleware ==>", header);
+    // 2. Split Token
+    const token = header.split(" ")[1];
+
+    // 3. Verify Token
+    jwt.verify(token, process.env.PRIVATE_KEY, (error, decode) => {
+      // console.log("error", error);
+      // console.log("decode", decode);
+      if (error) {
+        createError(401, "Token is Invalid !");
+      }
+
+      req.user = decode;
+      next()
+    });
+  } catch (error) {
+    next(error);
   }
-  next()
 }
